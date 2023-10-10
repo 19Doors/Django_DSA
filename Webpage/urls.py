5b0f41033c838ddcfb2321b366421fd871c510b7
Rishit-Ranjan <143101828+Rishit-Ranjan@users.noreply.github.com>
2023-10-10 12:17:41 +0530

    Update urls.py

--- Webpage/urls.py
+++ Webpage/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("",views.index, name="index"),
     path("contact",views.contact, name="contact"),
-    path("about",views.about, name="about")
+    path("about",views.about, name="about"),
+ #   path("datastructures",views.data_structure, name="data_structure"),
+ #   path("algorithms",views.algorithms, name="algorithms")
 ]
