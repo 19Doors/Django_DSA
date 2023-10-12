0d1631f04027b9ef4f39e7832d04054b3135b643
19Doors <sakaarsri1904@gmail.com>
2023-10-12 10:37:25 +0530

    Done

--- Webpage/urls.py
+++ Webpage/urls.py
@@ -1,8 +1,12 @@
 from django.urls import include,path
 from . import views
+from algorithms import views as algoviews
+from data_structure import views as datastrucviews
 
 urlpatterns = [
     path("",views.index, name="index"),
+    path("algorithms", algoviews.algorithm_view, name="algorithms"),
+    path("datastructures", datastrucviews.data_structure_view, name="data_structures"),
     path("contact",views.contact, name="contact"),
     path("about",views.about, name="about"),
  #   path("datastructures",views.data_structure, name="data_structure"),
