1e76734e4dcd4baa206b986fb1720644dc463181
19Doors <sakaarsri1904@gmail.com>
2024-07-27 16:16:06 +0530

    DEPLOY

--- AlgoViz/settings.py
+++ AlgoViz/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-t8z4%p0ge)=qjbnrs_js9$sjvbqyhns1@wrc@n@0*jlz-n4==p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'Webpage.apps.WebpageConfig',
     'algorithms.apps.AlgorithmsConfig'
 ]
@@ -120,13 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = "staticfiles"
+if os.environ.get('VERCEL'):
+    STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
