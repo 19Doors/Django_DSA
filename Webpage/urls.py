db25c28e4cc762ef546ef5f16b5720c6fc731053
19Doors <sakaarsri1904@gmail.com>
2024-07-27 16:58:30 +0530

    #$

--- Webpage/urls.py
+++ Webpage/urls.py
@@ -5,11 +5,11 @@ from data_structure import views as datastrucviews
 
 urlpatterns = [
     path("",views.index, name="index"),
-    path("algorithms",include('algorithms.urls')), 
-    path("algorithms", algoviews.algorithm_view, name="algorithms"),
-    path("datastructures", datastrucviews.data_structure_view, name="data_structures"),
-    path("contact",views.contact, name="contact"),
-    path("about",views.about, name="about"),
+    # path("algorithms",include('algorithms.urls')), 
+    # path("algorithms", algoviews.algorithm_view, name="algorithms"),
+    # path("datastructures", datastrucviews.data_structure_view, name="data_structures"),
+    # path("contact",views.contact, name="contact"),
+    # path("about",views.about, name="about"),
  #   path("datastructures",views.data_structure, name="data_structure"),
  #   path("algorithms",views.algorithms, name="algorithms")
 ]
